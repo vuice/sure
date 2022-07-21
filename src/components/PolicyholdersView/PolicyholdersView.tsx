9f6ac64d2a268b63ee4b214c90d0a6d2a1c9c999
Nicholas Vu <nicholas.vu@gmail.com>
2022-07-21 03:34:55 -0700

    [Challenge #6] Render result of API GET request in PolicyholdersView

--- src/components/PolicyholdersView/PolicyholdersView.tsx
+++ src/components/PolicyholdersView/PolicyholdersView.tsx
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
+import { mockPolicyHoldersList } from '../../constants/mockPolicyHoldersData';
 import InfoTable from '../InfoTable';
 
-const testRows = Array(5)
-  .fill(null)
-  .map((_, i) => ({
-    key: `KEY ${i}`,
-    value: 'VALUE',
-  }));
+type TPolicyHoldersList = {
+  policyHolders: { key: string; value: string | number }[][];
+};
 
 function PolicyholdersView() {
   const apiUrl = 'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders';
+  const defaultPolicyHoldersList: TPolicyHoldersList = { policyHolders: [[]] };
+
+  const [policyHoldersList, setPolicyHoldersList] = useState(defaultPolicyHoldersList);
 
   useEffect(() => {
     fetch(apiUrl)
@@ -25,18 +26,67 @@ function PolicyholdersView() {
         return rawData;
       })
       .then((rawData) => {
+        const displayData: TPolicyHoldersList = { policyHolders: [[]] };
+        const formattedData = rawData.policyHolders.map((
+          {
+            name = '',
+            age = 0,
+            address = {
+              line1: '',
+              line2: undefined,
+              city: '',
+              state: '',
+              postalCode: '',
+            },
+            phoneNumber = '',
+            isPrimary = true,
+          }) => ([
+            {
+              key: 'Name',
+              value: `${name}`,
+            },
+            {
+              key: 'Age',
+              value: Number(`${age}`),
+            },
+            {
+              key: 'Address',
+              value: `${address.line1}, ${address.line2 ? address.line2 + ', ' : ''}${address.city}, ${address.state}, ${address.postalCode}`,
+            },
+            {
+              key: 'Phone number',
+              value: `${phoneNumber}`,
+            },
+            {
+              key: 'Primary policy holder?',
+              value: `${isPrimary ? 'Yes' : 'No'}`,
+            },
+          ])
+        );
+
+        displayData.policyHolders = formattedData;
+
         console.log(0, 'fetched raw data:', rawData);
+        console.log(0, 'formatted data:', formattedData);
+        console.log(0, 'display data:', displayData);
+
+        setPolicyHoldersList(displayData);
       })
       .catch((error) => {
         console.error(0, error);
       });
   }, []);
 
+  console.log(10, 'formatted data mockPolicyHoldersList:', mockPolicyHoldersList);
+  console.log(10, 'formatted data policyHoldersList:', policyHoldersList);
+
   return (
     <Box sx={{ textAlign: 'center' }}>
-      <InfoTable header="Policy Holders" rows={testRows} />
+      {policyHoldersList.policyHolders.map((_, i) => (
+        <InfoTable key={i} header={(i === 0) ? "Policy Holders" : ""} rows={policyHoldersList.policyHolders[i]} />
+      ))}
     </Box>
   );
-}
+};
 
 export default PolicyholdersView;
---
+++ src/constants/mockPolicyHoldersData.ts
@@ -0,0 +1,50 @@
+const mockPolicyHoldersList = {
+  policyHolders: [
+    [
+      {
+        key: 'Name',
+        value: 'Nicholas Vu',
+      },
+      {
+        key: 'Age',
+        value: 123,
+      },
+      {
+        key: 'Address',
+        value: `123 Anywhere Lane, Suite 123, My City, My State, 12345`,
+      },
+      {
+        key: 'Phone number',
+        value: '+1-999-999-9999',
+      },
+      {
+        key: 'Primary policy holder?',
+        value: 'No',
+      },
+    ],
+    [
+      {
+      key: 'Name',
+      value: '2 Nicholas Vu',
+      },
+      {
+        key: 'Age',
+        value: 2123,
+      },
+      {
+        key: 'Address',
+        value: `2 123 Anywhere Lane, Suite 123, My City, My State, 12345`,
+      },
+      {
+        key: 'Phone number',
+        value: '2 +1-999-999-9999',
+      },
+      {
+        key: 'Primary policy holder?',
+        value: '2 No',
+      },
+    ]
+  ]
+};
+
+export { mockPolicyHoldersList };
